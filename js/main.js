755670e01cc6024ad68844520c419a578e5e0eca
Rayan-Alsaleh <52420277+Rayan-Alsaleh@users.noreply.github.com>
2019-09-09 21:32:57 +0300

    First comment of final project

---
+++ js/main.js
@@ -0,0 +1,89 @@
+function scrollFooter(scrollY, heightFooter)
+{
+    console.log(scrollY);
+    console.log(heightFooter);
+
+    if(scrollY >= heightFooter)
+    {
+        $('footer').css({
+            'bottom' : '0px'
+        });
+    }
+    else
+    {
+        $('footer').css({
+            'bottom' : '-' + heightFooter + 'px'
+        });
+    }
+}
+
+$(window).load(function(){
+    var windowHeight        = $(window).height(),
+        footerHeight        = $('footer').height(),
+        heightDocument      = (windowHeight) + ($('.content').height()) + ($('footer').height()) - 20;
+
+    // Definindo o tamanho do elemento pra animar
+    $('#scroll-animate, #scroll-animate-main').css({
+        'height' :  heightDocument + 'px'
+    });
+
+    // Definindo o tamanho dos elementos header e conteúdo
+    $('header').css({
+        'height' : windowHeight + 'px',
+        'line-height' : windowHeight + 'px'
+    });
+
+    $('.wrapper-parallax').css({
+        'margin-top' : windowHeight + 'px'
+    });
+
+    scrollFooter(window.scrollY, footerHeight);
+
+    // ao dar rolagem
+    window.onscroll = function(){
+        var scroll = window.scrollY;
+
+        $('#scroll-animate-main').css({
+            'top' : '-' + scroll + 'px'
+        });
+        
+        $('header').css({
+            'background-position-y' : 50 - (scroll * 100 / heightDocument) + '%'
+        });
+
+        scrollFooter(scroll, footerHeight);
+    }
+});
+
+
+
+const panels = document.querySelectorAll('.panel');
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+panels.forEach(function(panel) {
+  panel.addEventListener('click', toggleOpen);
+})
+
+// function toggleActive(e) {
+//   if(e.propertyName.includes('flex')) { //can be every property of this element that had changed(transisioned)
+//     this.classList.toggle('open-active');
+//   }
+// }
+function toggleActive() {
+  this.classList.toggle('open-active');
+}
+panels.forEach(function(panel) {
+  panel.addEventListener('transitionend', toggleActive);
+})
+
+
+
+
+
+
+
+// tt
+
+
+
